953bf40cdbbdf646550e6ec74c78e8a89285a2f3
Andrew Dirksen <andrew.dirksen@formal.tech>
2017-05-30 13:23:43 -0700

    got graphing to work, it was 'chart.js', not 'chart' and not 'chartjs'

--- finalproj/site/index.js
+++ finalproj/site/index.js
@@ -1,56 +1,41 @@
-const vis = require('vis')
-const Chart = require('chart')
+const Chart = require('chart.js')
 
-function showTable(table, parent) {
-  const t = document.createElement('table');
-  parent.appendChild(t);
-  table.forEach((row, i) => {
-    const r = t.insertRow(i);
-    row.forEach((cell, j) => {
-      r.insertCell(j).textContent = cell;
+module.exports = {
+  fillTable: function(table, tds) {
+    tds.forEach(row => {
+      const r = table.insertRow()
+      row.forEach(cell => {
+        r.insertCell().textContent = cell;
+      });
     });
-  });
+  },
+
+  loadPrices: function() {
+    return fetch('prices.json')
+      .then(a => a.json())
+  },
+
+  ctxIn: function(idname) {
+    const parent = document.getElementById(idname)
+    const canvas = document.createElement('canvas')
+    parent.appendChild(canvas)
+    return canvas.getContext("2d")
+  }
 }
 
-function loadPrices() {
-  return fetch('prices.json')
-    .then(a => a.json())
-}
+// loadPrices()
+//   .then(prices => {
+//     // show table
+//     // showTable([
+//     //   ["Time (years)", "Average Oregon home price (thousands of dollars)"]
+//     // ].concat(
+//     //   prices.map(p => [p.x, p.y])
+//     // ), document.getElementById('data_table'))
+// 
+//   })
 
-function ctxIn(idname) {
-  const parent = document.getElementById(idname)
-  const canvas = document.createElement('canvas')
-  parent.appendChild(canvas)
-  return canvas.getContext("2d")
-}
-
-loadPrices()
-  .then(prices => {
-    // show table
-    // showTable([
-    //   ["Time (years)", "Average Oregon home price (thousands of dollars)"]
-    // ].concat(
-    //   prices.map(p => [p.x, p.y])
-    // ), document.getElementById('data_table'))
-
-    // console.log(prices[0])
-    // // show plot
-    // const container = document.getElementById('initial_plot');
-    // // const dataset = new vis.DataSet(prices);
-    // const options = {
-    //   start: 1975.0,
-    //   end: 2016.75
-    // };
-    // const graph2d = new vis.Graph2d(container, prices, options);
-    // console.log(graph2d)
-    const c = ctxIn('visualization')
-    c.line
-    console.log(prices)
-
-    let myLineChart = new Chart(ctxIn('initial_plot'), {
-      type: 'line',
-      data: prices
-    })
-
-    console.log(myLineChart);
-  })
+// document.onload = function() {
+//   
+//   let ctx = document.getElementById("myChart").getContext("2d");
+// 
+//
