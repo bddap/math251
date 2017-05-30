4388072602c75f4f7eaae2b40be80eac28fd401f
Andrew Dirksen <andrew.dirksen@formal.tech>
2017-05-30 08:55:25 -0700

    make xsys use the loader from plothouse

--- finalproj/xsys.py
+++ finalproj/xsys.py
@@ -1,43 +1,12 @@
-import csv
+#! /usr/bin/env python3
+
+from plothouse import ds
 import json
 import sys
 
-def ds():
-    d = list(csv.DictReader(open('HPI_master.csv'), quoting=csv.QUOTE_MINIMAL))
-
-    # only oregon
-    d = [l for l in d if l['place_id'] == 'OR']
-
-    d = [l for l in d if
-        (l['hpi_type'], l['hpi_flavor']) == ('traditional', 'all-transactions')]
-
-    for l in d:
-        # all our data is state level now
-        del l['level']
-        # all our frequencies are quarterly
-        del l['frequency']
-        # place_name and place_id are always Oregon and OR
-        del l['place_name']
-        del l['place_id']
-        # hpi_type and hpi_flavor are always traditional all-transactions
-        del l['hpi_type']
-        del l['hpi_flavor']
-
-        # use only one axis for time
-        l['yr'] = float(l['yr']) + (float(l['period']) - 1) / 4
-        del l['period']
-
-        # index_sa is not always defined
-        del l['index_sa']
-
-    return list(zip(
-        [float(l['yr']) for l in d],
-        [float(l['index_nsa']) for l in d]
-    ))
-
 if __name__ == '__main__':
     json.dump([
-        {'x': x, 'y': y} for x, y in ds()
+        {'x': x, 'y': y} for x, y in zip(*ds())
     ], sys.stdout)
 
-# python3 xsys.py | jq . > site/prices.json
+# ./xsys.py | jq . > site/prices.json
